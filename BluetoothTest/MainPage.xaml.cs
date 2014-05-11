949d8778d874c35317c96be44047c9987cb46fe2
n8n8n88n8n8nn88n <nate.olds@gmail.com>
2014-05-10 23:15:58 -0700

    A2: blueTOOTH wow!  I'm a big kid now!

--- BluetoothTest/MainPage.xaml.cs
+++ BluetoothTest/MainPage.xaml.cs
@@ -1,4 +1,107 @@
-﻿using System;
+﻿///////////////////////////////////PASTE INTO ARDUINO.CC/////////////////////////////////////////
+//Setup is simple, and boilerplate:
+
+//#include "foneastrapins.h"
+//void setup(void) {
+//  //set up the HW UART to communicate with the BT module
+//  Serial.begin(38400);
+
+//  // Provide power to BT
+//  pinMode(BT_PWR_PIN,OUTPUT);
+//  digitalWrite(BT_PWR_PIN,HIGH);
+
+//  // Turn on the red light
+//  pinMode(RED_LED_PIN,OUTPUT);
+//  digitalWrite(RED_LED_PIN, HIGH);
+//}
+
+///////////////////////////////////////////////////////ADDTL CODE/////////////////////////////////
+
+//Programming the Arduino is an exercise in Simplicity
+//It’s straight C/C++, and really easy C++ at that!
+
+//Need two functions at minimum:
+//setup() and loop()
+
+//Do Input/Output with the Serial object
+//    Serial.print(“Hello!”)
+//    if( Serial.available() > 0 )
+//      char key = Serial.read()
+
+//Output beeps asynchronously with tone()
+//    tone( BUZZER_PIN, 1000, 100 );
+//    delay( 100 );
+
+//////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+//////////////////////////////////////////////////////BBT/////////////////////////////////////////
+//Once you turn the Bluetooth on, it’s just “available”
+//No “initialization” step, other than ensuring that ID_CAP_NETWORKING and ID_CAP_PROXIMITY are both checked
+
+//Use the PeerFinder class to search nearby devices
+//Used to find not only other BT devices, but also specific applications
+//Operates not only on BT, but also NFC and Wifi
+//We’re using only BT, so we’ll make sure to specify that
+
+//Once you’ve got a Peer, open a StreamSocket
+//The most basic TCP/Bluetooth communication method
+//Sends a continuous stream of data
+//This is as opposed to Datagram-based communication (e.g. UDP)
+
+
+//Working with Data Streams can be tricky
+//This idealized “stream” of data is chunked up into packets
+//These packets are then sent to the BT radio and transmitted
+//This is done transparently by the operating system
+//The receiving end is notified at the packet level
+//There is no way of knowing how much data to wait for
+//Unless we invent a protocol to give us this knowledge!
+
+//Protocols are easy and neat
+//Example protocol:
+//First 4 bytes of every message is an integer with size of following data
+//Next N bytes are the data, where N is the value of the original integer
+//Interpreting this data is completely up to your application
+//Another protocol:
+//Print out data willy-nilly, know it’s done with a newline
+//Read it in, appending to a buffer until you read in a newline
+
+
+
+//First, we tell PeerFinder to find all paired BT devices
+//   PeerFinder.AlternateIdentities["Bluetooth:PAIRED"] = ""
+
+//Next, get a list of all peers that satisfy those criteria:
+//   var peers = await PeerFinder.FindAllPeersAsync();
+
+//Find the peer we want:
+//   PeerInformation peerInfo = peers.FirstOrDefault(x =>
+//                                   x.DisplayName.Contains("FoneAstra"));
+//Connect to that peer if not null
+//   s = new StreamSocket();
+//   await s.ConnectAsync(peerInfo.HostName, "1");
+
+//And now we have a socket connected!
+
+
+//To send/receive data, we use Data{Writer,Reader}
+//We build these out of the StreamSocket
+//   DataWriter output = new DataWriter(sock.OutputStream);
+//   DataReader input = new DataReader(sock.InputStream);
+
+//To read data in, first Load it, then Read it:
+//   await input.LoadAsync(4);
+//   input.ReadBytes(byteBuffer);
+//   int length = BitConverter.ToInt32(byteBuffer, 0);
+
+//To write data out, Write it then Store it:
+//   byte[] data = { 100, 253, 5, 67 };
+//   output.WriteBytes(data);
+//   output.StoreAsync();
+
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
